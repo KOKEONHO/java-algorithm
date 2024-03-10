d74d0c8c32357d8f7fd1eb2a324a40c4ea39f5be
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-03-10 13:20:16 +0900

    LeetCode 344. Reverse String (my solution)

---
+++ mysolution/leetcode/easy/ReverseString.java
@@ -0,0 +1,18 @@
+package leetcode.easy;
+
+public class ReverseString {
+
+	public void reverseString(char[] s) {
+
+		for (int i = 0; i < s.length / 2; i++) {
+			if (s[i] == s[s.length - 1 - i]) {
+				continue;
+			}
+			char temp = s[i];
+			s[i] = s[s.length - 1 - i];
+			s[s.length - 1 - i] = temp;
+		}
+
+	}
+
+}
