f1a00bab7311908c34b8a1e968c1c3a054d2b1b6
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-08-12 17:35:39 +0900

    LeetCode 88. Merge Sorted Array (my solution)

---
+++ mysolution/leetcode/easy/MergeSortedArray.java
@@ -0,0 +1,21 @@
+package leetcode.easy;
+
+public class MergeSortedArray {
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+		int[] nums1Copy = new int[m];
+		System.arraycopy(nums1, 0, nums1Copy, 0, m);
+
+		int p1 = 0;
+		int p2 = 0;
+
+		for (int p = 0; p < m + n; p++) {
+
+			if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+				nums1[p] = nums1Copy[p1++];
+			} else {
+				nums1[p] = nums2[p2++];
+			}
+		}
+	}
+}
