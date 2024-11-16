6977a4f423c73137022652e772b304dc47ab2fad
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-11-16 16:22:46 +0900

    LeetCode 200. Number of Islands (my solution)

--- mysolution/leetcode/medium/NumberOfIslands.java
+++ mysolution/leetcode/medium/NumberOfIslands.java
@@ -2,40 +2,42 @@ package leetcode.medium;
 
 public class NumberOfIslands {
 
-	static int result;
-	static int maxRow, maxCol;
-	static int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-	static boolean[][] visited;
+	int row = 0;
+	int col = 0;
+	boolean[][] visited;
+	int[][] directionArray = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // up down left right
 
 	public int numIslands(char[][] grid) {
 
-		result = 0;
+		int islandCount = 0;
+		row = grid.length;
+		col = grid[0].length;
+		visited = new boolean[row][col];
 
-		maxRow = grid.length;
-		maxCol = grid[0].length;
-		visited = new boolean[maxRow][maxCol];
-
-		for (int i = 0; i < maxRow; i++) {
-			for (int j = 0; j < maxCol; j++) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
 				if (grid[i][j] == '1' && !visited[i][j]) {
-					dfs(i, j, grid);
-					result++;
+					dfs(grid, i, j);
+					islandCount++;
 				}
 			}
 		}
 
-		return result;
+		return islandCount;
+
 	}
 
-	private static void dfs(int row, int col, char[][] grid) {
-		visited[row][col] = true;
-		for (int[] direction : directions) {
-			int vertical = row + direction[0];
-			int horizontal = col + direction[1];
-			if (vertical < maxRow && vertical > -1 && horizontal < maxCol && horizontal > -1
-				&& !visited[vertical][horizontal] && grid[vertical][horizontal] == '1') {
-				dfs(vertical, horizontal, grid);
+	private void dfs(char[][] grid, int i, int j) {
+
+		visited[i][j] = true;
+		for (int[] direction : directionArray) {
+			if ((i + direction[0] < row && i + direction[0] >= 0) && (j + direction[1] < col && j + direction[1] >= 0)) {
+				if (grid[i + direction[0]][j + direction[1]] == '1' && !visited[i + direction[0]][j + direction[1]]) {
+					dfs(grid, i + direction[0], j + direction[1]);
+				}
 			}
 		}
+
 	}
+
 }
