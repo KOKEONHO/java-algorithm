b9d53c7956211ec581348adc0be63cefdb811c8b
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-02-27 12:45:12 +0900

    Programmers 타겟 넘버 수정

--- mysolution/programmers/level2/타겟_넘버.java
+++ mysolution/programmers/level2/타겟_넘버.java
@@ -12,7 +12,24 @@ public class 타겟_넘버 {
         dfs(0, numbers, target, 0);
 
         Deque<Integer> deque = new ArrayDeque<>();
+        int start = 0;
 
+        deque.addLast(start);
+
+        for (int number : numbers) {
+            int dequeSize = deque.size();
+            for (int i = 0; i < dequeSize; i++) {
+                int value = deque.pollFirst();
+                deque.addLast(value + number);
+                deque.addLast(value - number);
+            }
+        }
+
+        while (!deque.isEmpty()) {
+            if (target == deque.pollFirst()) {
+                result++;
+            }
+        }
 
         return result;
 
