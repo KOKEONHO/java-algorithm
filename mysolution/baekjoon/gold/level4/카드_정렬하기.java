3088ac92569999cf1428fc303b4d0869c5504f2d
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-03-13 11:36:55 +0900

    Baekjoon Gold4 1751. 카드 정렬하기

---
+++ mysolution/baekjoon/gold/level4/카드_정렬하기.java
@@ -0,0 +1,34 @@
+package baekjoon.gold.level4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;s
+
+public class 카드_정렬하기 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        int result = 0;
+        int n = Integer.parseInt(br.readLine());
+
+        while (n-- > 0) {
+            int card = Integer.parseInt(br.readLine());
+            minHeap.add(card);
+        }
+
+        while (minHeap.size() > 1) {
+            int first = minHeap.poll();
+            int second = minHeap.poll();
+            int total = first + second;
+            result += total;
+            minHeap.add(total);
+        }
+
+        System.out.println(result);
+
+    }
+}
