f72e298da7aa9d8f3c2cd68d5b699679b0508ae7
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-03 22:08:54 +0900

    Baekjoon Gold5 9251. LCS (tabulation을 활용한 Bottom-Up)

---
+++ mysolution/baekjoon/gold/level5/LCS_Bottom_Up.java
@@ -0,0 +1,34 @@
+package baekjoon.gold.level5;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class LCS_Bottom_Up {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int n = str1.length();
+        int m = str2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][m]);
+
+    }
+}
