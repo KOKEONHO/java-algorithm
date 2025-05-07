9c49165ac5b74ff46971b7af0066c655483aeb10
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-07 13:17:50 +0900

    Baekjoon Silver2 11055. 가장 큰 증가하는 부분 수열 (Bottom-Up)

---
+++ mysolution/baekjoon/silver/level2/가장_큰_증가하는_부분_수열.java
@@ -0,0 +1,41 @@
+package baekjoon.silver.level2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 가장_큰_증가하는_부분_수열 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] sequence = new int[N];
+        int[] dp = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            sequence[i] = Integer.parseInt(st.nextToken());
+            dp[i] = sequence[i];
+        }
+
+        int result = dp[0];
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (sequence[i] > sequence[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + sequence[i]);
+                    result = Math.max(result, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
